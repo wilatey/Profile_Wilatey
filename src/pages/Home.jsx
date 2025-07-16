22708caf678f215f61e40b82b07ad7fb92fc1b44
Llatey <115705225+Llatey@users.noreply.github.com>
2025-07-16 17:37:33 +0800

    react web

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { ThemeToggle } from "../components/ThemeToggle";
+
+export const Home = () => {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden" class="dark:bg-black">
+            {/*Theme Toggle */}
+            <ThemeToggle />
+            {/* Background Effects */}
+
+
+            {/* Navbar */}
+
+
+            {/* Footer */}
+        </div>
+    );
+};
