c9507a054fc6238f6495a27b6fd798ed770b4afc
Wilatey <wilsonlam085@gmail.com>
2025-07-30 17:48:41 +0800

    New update on About, Project and Skills sections

---
+++ src/components/Project.jsx
@@ -0,0 +1,95 @@
+import { ExternalLink, GithubIcon } from "lucide-react";
+
+const projects = [
+    {
+        id: 1,
+        title: "Web Collaboration WhiteBoard",
+        description: "Multiple users editing for day-planning",
+        github: "https://github.com/wilatey/Web-Collabortion-Whiteboard.git",
+        image: "#",
+        tags: ["React", "TailwindCSS", "WebSocket"]
+    },
+    {
+        id: 2,
+        title: "Wallet transaction viewer",
+        description: "A Web application of connection in crypto wallet",
+        github: "https://github.com/wilatey/Wallet-transaction-viewer.git",
+        image: "#",
+        tags: ["React, TailwindCSS", "Web"]
+    },
+    {
+        id: 3,
+        title: "Bicycle tracker application",
+        description: "Tracking system in bicycle for mobile users",
+        github: "https://github.com/wilatey/Mobile-Bicycle-tracker.git",
+        image: "#",
+        tags: ["Java, Andriod Studio"]
+    }
+];
+
+
+export const Project = () => {
+    return (
+        <section id="projects" className="py-24 px-24 relative">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-font mb-4 text-center">
+                    Featured <span className="text-primary"> Projects </span>
+                </h2>
+
+                <p className="text-center text-muted-foreground mb-12 max-w-xl mx-auto">
+                    Here are my projects base. Each of them are crafted on
+                    specializing to user experience, detail, performance, and
+                    so on with attention.
+                </p>
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+                    {projects.map((project, key) => (
+                        <div
+                            key={key}
+                            className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover"
+                        >
+                            <div className="h-48 overflow-hidden">
+                                <img 
+                                    src={project.image} 
+                                    alt={project.title} 
+                                    className="w-full h-full object-cover transition-transform duration-500" group-hover:scale-110
+                                />
+                            </div>
+                        
+                        <div className="p-6">
+                            <div className="flex flex-wrap gap-2 mb-4">
+                                {project.tags.map((tag) => (
+                                    <span className="px-2 py-1 text-xs font-medium rounded-full bg-primary/20 text-secondary-foreground">
+                                        {tag}
+                                    </span>
+                            ))}
+                            </div>  
+                        </div>
+                        
+                        <h3 className="text-xl font-semibold mb-1">
+                            {project.title}
+                        </h3>
+
+                        <p className="text-muted-foreground test-sm mb-4">
+                            {project.description}
+                        </p>
+
+                        <div className="flex justify-between items-center">
+                            <div className="flex ml-3 space-x-3">
+                                <a 
+                                href={project.github}
+                                target="_blank"
+                                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                                > 
+                                    <GithubIcon size={20}/>
+                                </a>
+                            </div>
+                        </div>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/components/Skills.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import { cn } from "../lib/utili";
+
+const skill = [
+    //Front-end
+    { name: "HTML/CSS", level: 80, category: "Frontend" },
+    { name: "JavaScript", level: 50, category: "Frontend" },
+    { name: "React", level: 30, category: "Frontend" },
+    { name: "Tailwind CSS", level: 30, category: "Frontend" },
+    { name: "Next.js", level: 20, category: "Frontend" },
+
+
+    //Back-end
+    { name: "C++", level: 65, category: "Backend" },
+    { name: "Java", level: 50, category: "Backend" },
+    { name: "Python", level: 40, category: "Backend" },
+    { name: "Node.js", level: 20, category: "Backend" },
+    { name: "MySQL", level: 15, category: "Backend" },
+
+
+    //Tools
+    { name: "Git/GitHub", level: 50, category: "Tool" },
+    { name: "Docker", level: 30, category: "Tool" },
+    { name: "VScode", level: 80, category: "Tool" },
+];
+
+const category = ["all", "Frontend", "Backend", "Tool"];
+
+export const Skills = () => {
+    const [activityCategory, setActiveCategory] = useState("all");
+
+    const filteredSkills = skill.filter(
+        (skill) => activityCategory === "all" || skill.category === activityCategory
+    );
+
+    return (
+        <section id="skills" className="py-24 px-4 relative bg-secondary/30">
+            <div className="container mx-auto max-w-5xl">
+                <h2 className="text-3xl md:text-4xl font-bold mb-12 text-center">
+                    My <span className="text-primary"> Skills </span>
+                </h2>
+
+                <div className="flex flex-wrap justify-center gap-4 mb-12">
+                    {category.map((category, key) => (
+                        <button
+                            key={key}
+                            onClick={() => setActiveCategory(category)}
+                            className={cn(
+                                "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                                activityCategory === category ? "bg-primary text-primary-foreground" : "bg-secondary/70 text-foreground hover:bg-secondary"
+                            )}
+                        >
+                            {category}
+
+                        </button>
+                    ))}
+                </div>
+
+
+                <div className="grid md:grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+                    {filteredSkills.map((skills, key) => (
+                        <div
+                            key={key}
+                            className="bg-card p-6 rounded-lg shadow-xs card-hover"
+                        >
+                            <div className="text-left mb-4">
+                                <h3 className="font-semibold text-lg"> {skills.name} </h3>
+                            </div>
+                            <div className="w-full bg-secondary/50 h-2 rounded-full overflow-hidden">
+                                <div
+                                    className="bg-primary h-2 rounded-full origin-left animate-[glow_1.5s_ease-out]"
+                                    style={{ width: skills.level + "%" }}
+                                />
+                            </div>
+                            <div className="text-right mt-1">
+                                <span className="text-sm text-muted-foreground"> {skills.level}% </span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    )
+
+}
